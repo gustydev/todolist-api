c25ab50f5c89f26ba198970660ed0f78ba1ae28e
gusty <gustavodiorlando@gmail.com>
2024-11-22 14:09:13 -0300

    Add error handler and user router with register and login

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
+const createError = require('http-errors');
+
+const userRoute = require("./routes/userRoute")
 
 const mongoose = require('mongoose');
 mongoose.set('strictQuery', 'false')
@@ -19,5 +22,31 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'))
 
+// Rotas da aplicação
+app.use('/user', userRoute)
+
+// cria erro 404 quando rota não é encontrada
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// Gerenciador de erros (captura erros da aplicação e retorna eles)
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Internal Server Error';
+
+  const error = {
+    statusCode,
+    message,
+    ...err
+  };
+
+  if (process.env.NODE_ENV === 'development') {
+    console.error(error)
+  }
+
+  res.status(statusCode).json(error)
+});
+
 const port = process.env.PORT || 3000;
-app.listen(port, () => {console.log(`App listening on port ${port}`)})
+app.listen(port, () => {console.log(`Servidor rodando na porta ${port}`)})
---
+++ routes/userRoute.js
@@ -0,0 +1,126 @@
+require('dotenv').config();
+require('mongoose')
+
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+
+const User = require('../models/user')
+const { body, validationResult } = require('express-validator');
+
+const router = require('express').Router();
+
+router.post('/register', [
+    // Validar entradas do usuário com express validator
+    body('username')
+    .isString()
+    .withMessage('Nome de usuário precisa ser uma string')
+    .trim()
+    .isLength({min: 4, max: 30})
+    .withMessage('Nome de usuário precisa ter entre 4 e 30 caracteres')
+    .custom(async (value) => {
+        const user = await User.findOne({username: value})
+        if (user) {
+            throw new Error('Nome de usuário indisponível, por favor tentar outro.')
+        } 
+    }),
+
+    body('password')
+    .isString()
+    .withMessage('Senha precisa ser uma string')
+    .trim()
+    .isLength({min: 8})
+    .withMessage('Senha precisa ter no mínimo 8 caracteres'),
+
+    async(req, res, next) => {
+        try {
+            const errors = validationResult(req);
+        
+            if (!errors.isEmpty()) {
+                // Se tiver erro, retorná-lo e interromper processo de registração
+                const error = new Error('Falha ao validar entradas de usuário')
+                error.statusCode = 400;
+                error.details = errors.array();
+
+                return next(error);
+            }
+            
+            const hash = await bcrypt.hash(req.body.password, 10);
+            // adiciona criptografia à senha (LGPD)
+    
+            const user = new User({
+                username: req.body.username,
+                password: hash
+            });
+    
+            await user.save();
+
+            return res.status(200).json({message: `Usuário ${user.username} criado com sucesso`})
+        } catch (err) {
+            next(err) // envia erros ao gerenciador de erros do app.js
+        }
+    }
+]);
+
+router.post('/login', [
+    body('username')
+    .isString()
+    .withMessage('Nome de usuário precisa ser string')
+    .trim()
+    .isLength({min: 4, max: 30})
+    .withMessage('Nome de usuário precisa ter entre 4 e 30 caracteres')
+    .custom(async (value) => {
+        const user = await User.findOne({username: value})
+        if (!user) {
+            throw new Error("Usuário não encontrado")
+        }
+    }),
+
+    body('password')
+    .isString()
+    .withMessage('Senha precisa ser uma string')
+    .trim()
+    .isLength({min: 8})
+    .withMessage('Senha precisa ter no mínimo 8 caracteres')
+    .custom(async (value, {req}) => {
+        const user = await User.findOne({username: req.body.username})
+        if (!user) {
+            return false;
+        }
+
+        const match = await bcrypt.compare(value, user.password);
+        // verifica se senha está correta
+
+        if (!match) { throw new Error('Senha incorreta, por favor tentar novamente')}
+    }),
+
+    async(req, res, next) => {
+        try {
+            const errors = validationResult(req);
+        
+            if (!errors.isEmpty()) {
+                const error = new Error('Falha ao validar entradas de usuário')
+                error.statusCode = 400;
+                error.details = errors.array();
+
+                return next(error);
+            }
+
+            const user = await User.findOne({username: req.body.username}).select('-password');
+            // dados do usuário, omitindo a senha hasheada por segurança
+
+            const token = jwt.sign({id: user._id}, process.env.SECRET, {expiresIn: '30d'});
+            // json web token utilizado pra gerenciar autenticação/autorização do site, vence em 30 dias
+            // ele é usado nas requests http num header 'Authorization' com valor 'Bearer [token]'
+
+            return res.status(200).json({
+                msg: `Logado com sucesso! Token vence em 30 dias`,
+                token,
+                user
+            })
+        } catch (err) {
+            next(err);
+        }
+    }
+])
+
+module.exports = router;
